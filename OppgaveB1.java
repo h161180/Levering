b223b7373ecdf8ed6af4d33dc93d9a7899f67a60
h161180 <89388506+h161180@users.noreply.github.com>
2021-09-10 13:13:51 +0200

    Add files via upload

---
+++ OppgaveB1.java
@@ -0,0 +1,17 @@
+package no.hvl.dat100;
+
+import static java.lang.Math.*;
+import static javax.swing.JOptionPane.*;
+
+public class OppgaveB1 {
+
+	public static void main(String[] args) {
+
+		int radius = 8;
+		double volume = (4.0 / 3.0) * PI * (radius * radius * radius);
+
+		System.out.println("Volumet av kulen er: " + volume + "m^3");
+
+	}
+
+}
